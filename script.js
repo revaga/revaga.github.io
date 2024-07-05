430052ef196f297166a512378e5c71296ae21560
revaga <58123400+revaga@users.noreply.github.com>
2024-07-05 10:48:30 -0400

    update art tab, fix code, make time nicer

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ window.onload = dragElement(document.getElementById("guitar"))
 window.onload = dragElement(document.getElementById("lemonade"))
 window.onload = dragElement(document.getElementById("aframe"))
 window.onload = dragElement(document.getElementById("books"))
+window.onload = dragElement(document.getElementById("flexbox"))
+
 
 
 // Step 1: Define a function called `dragElement` that makes an HTML element draggable.
@@ -140,15 +142,16 @@ function initializeWindow(elementName) {
 }
 */
 
-//confetti
-
 
 function openNotes(element) {
+  //confetti
+  /*
   confetti({
     particleCount: 100,
     spread: 70,
     origin: { x: 0.05, y: 0.2 },
   });
+  */
   element.style.display = "flex";
   biggestIndex++;  // Increment biggestIndex by 1
   element.style.zIndex = biggestIndex;
@@ -156,21 +159,85 @@ function openNotes(element) {
  
 }
 
+
+let parent = document.getElementById("booklist")
+fetch("https://api.airtable.com/v0/appnG920EgeSDbgtT/tblAKQHEYpWXx3o49", 
+{headers:
+    {Authorization: "Bearer patpSYdx3Abw3h8h7.abd52d7dccadbb7102e7116d6a63ccf21006432cdd8e2ba529692e2b989570cb"}
+
+}).then(res=> res.json()).then(json => {
+let records = json.records.map(previousRecord =>{
+    return previousRecord.fields["Books"];       
+})
+let num = Math.floor(Math.random() * records.length)
+console.log(records[num])
+book_item = document.createElement("p")
+book_item.innerHTML = records[num]
+parent.appendChild(book_item)
+
+})
+
+
+
   let sketch = function(p) {
-    p.setup = function() {
-        let canvas = p.createCanvas(400, 400);
-        p.background(51);
-    };
+    let cnv;
+    let d;
+  p.setup = function() {
+  cnv = p.createCanvas(400, 400);
+  cnv.mouseOver(changeGray);
+  d = 10;
+}
 
-    p.draw = function() {
-      p.background(220);
+p.draw  = function() {
+  let ry = p.random(0, 400)
+  let rx = p.random(0, 400)
+  let d = p.random(5,40);
+  let check = 1;
+  let r; let b; let g;
+    let change = p.random(1,4)
+    if(change <= 1){
+      g = 66
+      b = 245
+      r = p.random(66,246)
+    }
+    else if(change <= 2){
+      g = p.random(66,246)
+      r = 66
+      b = 245
+    }
+    else{
+      b = p.random(66,246)
+      r = 66
+      g = 245
+    }
+  let t = p.random(10,225)
+  p.fill(r,g,b,t)
+  p.noStroke();
+  p.ellipse(rx, ry, d, d);
 
-      if(p.mouseIsPressed){
-        p.background(100)
-      }
+}
 
-    };
-};
+function changeGray() {
+  d = d + 10;
+  if (d > 100) {
+    d = 0;
+  }
+}
+
+}
 
 let p5canvas = new p5(sketch, 'artdiv');
   
+
+
+
+//flexbox styling
+function flexdirectionrow(){
+  let parent = document.getElementById("parentflex")
+  parent.style.flexDirection = "row"
+}
+
+function flexdirectioncol(){
+  let parent = document.getElementById("parentflex")
+  parent.style.flexDirection = "column"
+}
